046c2659358aa60b0ec48ff3c5486721bfb78271
RielJ <bulaybulay.rielj@gmail.com>
2024-05-27 14:01:26 +0800

    chore: added extra projects

--- src/pages/resume/constants/projects.ts
+++ src/pages/resume/constants/projects.ts
@@ -2,11 +2,27 @@ import { TProject } from '@/types'
 
 export const projects: TProject[] = [
   {
-    name: 'Rock, Paper, Scissors, Spock, and Lizard Web3 Game',
+    name: '⛽ Ethereum Gas Tracker',
+    link: 'https://gas-tracker.rielj.xyz/',
+    description: [
+      'This is a web-based tool that allows users to track the current gas prices on the Ethereum network. The tool provides a simple interface to view the current gas prices in Gwei and USD.',
+      'Using Modern Web Technologies such as Etherscan, ReactJS, Vite, tailwindcss, and shadcn UI.',
+    ],
+  },
+  {
+    name: '📹 Fabric Frame Capture and Playback',
+    link: 'https://fabric-capture.rielj.xyz',
+    description: [
+      'This is a web-based tool that allows users to capture and playback frames from an object in a canvas. The tool provides a simple interface to capture frames from a canvas element and then allows users to play back the frames.',
+      'Using Modern Web Technologies such as Fabric, ReactJS, Vite, tailwindcss, and shadcn UI.',
+    ],
+  },
+  {
+    name: '✂️ Rock, Paper, Scissors, Spock, and Lizard Web3 Game',
     link: 'https://rpssl.rielj.xyz',
     description: [
       'This is a web-based implementation of the popular game "Rock, Paper, Scissors" extended with two additional choices: "Spock" and "Lizard". The game allows players to compete against each other using Web3 technology.',
-      'Using Modern Web Technologies such as NextJS, wagmi.sh, Prisma, and shadcn UI.',
+      'Using Modern Web Technologies such as NextJS, wagmi.sh, tailwindcss, and shadcn UI.',
     ],
   },
 ]
