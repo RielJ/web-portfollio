fd31ea85044e8b33acdf87f5457fb2eb0c4b1be6
RielJ <bulaybulay.rielj@gmail.com>
2024-05-27 13:22:21 +0800

    chore: refactored links

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,19 @@
 import { Contacts, Footer } from '@/components'
 import { Hero, About, Experience, Portfolio, Navbar } from './sections'
+import { Link } from 'react-router-dom'
 
 type THomeProps = {}
 
 const Home = ({}: THomeProps) => {
-  const goToResume = () => {
-    window.location.href = '/resume'
-  }
   return (
     <>
       <div className="relative z-10">
-        <div
-          onClick={goToResume}
+        <Link
+          to="/resume"
           className="z-50 fixed bottom-0 right-0 w-[300px] h-16 transform -rotate-45 flex justify-center items-center bg-primary text-white py-2 shadow-lg hover:bg-blue-900 transition duration-300 cursor-pointer translate-x-[25%] -translate-y-1/2"
         >
           <span className="transform ">Resume</span>
-        </div>
+        </Link>
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -8,22 +8,19 @@ import {
   Projects,
   Skillset,
 } from './sections'
+import { Link } from 'react-router-dom'
 
 type TResumeProps = {}
 
 const Resume = ({}: TResumeProps) => {
-  const goToPortfolio = () => {
-    window.location.href = '/'
-  }
-
   return (
     <div className="relative z-10 flex flex-col gap-16 max-w-7xl w-full mx-auto mb-24">
-      <div
-        onClick={goToPortfolio}
+      <Link
+        to="/"
         className="z-50 fixed bottom-0 right-0 w-[300px] h-16 transform -rotate-45 flex justify-center items-center bg-white text-black py-2 shadow-lg hover:bg-gray-300 transition duration-300 cursor-pointer translate-x-[25%] -translate-y-1/2"
       >
         <span className="transform ">Portfolio</span>
-      </div>
+      </Link>
       <Header />
       <Separator />
       <About />
