e2cfc15f7d425ca7f482f8daa12b640aaa6c6cd1
RielJ <bulaybulay.rielj@gmail.com>
2024-05-27 13:49:20 +0800

    style: change link color

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,7 +10,7 @@ const Home = ({}: THomeProps) => {
       <div className="relative z-10">
         <Link
           to="/resume"
-          className="z-50 fixed bottom-0 right-0 w-[300px] h-16 transform -rotate-45 flex justify-center items-center bg-primary text-white py-2 shadow-lg hover:bg-blue-900 transition duration-300 cursor-pointer translate-x-[25%] -translate-y-1/2"
+          className="z-50 fixed bottom-0 right-0 w-[300px] h-16 transform -rotate-45 flex justify-center items-center bg-white text-black py-2 shadow-lg hover:bg-gray-300 transition duration-300 cursor-pointer translate-x-[25%] -translate-y-1/2"
         >
           <span className="transform ">Resume</span>
         </Link>
